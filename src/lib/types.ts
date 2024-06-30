1e3966e5c3e9731a312e62fef55543bc6cf8a3d6
shironxn <qshiro69@gmail.com>
2024-06-30 14:46:34 +0700

    feat: header card quote footer

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+export interface HeaderProps {
+  avatarURL: string;
+  title: string;
+  desc: string;
+}
+
+export interface CardProps {
+  icon: string;
+  title: string;
+  desc: string;
+  url: string;
+}
